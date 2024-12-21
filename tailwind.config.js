769e24067d356214faf3c68ef73b7ae581190b0b
Brian Smiley <briantsmiley42@gmail.com>
2024-12-20 19:20:38 -0500

    prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		keyframes: {
-  			'fade-in': {
-  				'0%': {
-  					opacity: '0'
-  				},
-  				'100%': {
-  					opacity: '1'
-  				}
-  			},
-  			'fade-out': {
-  				'0%': {
-  					opacity: '1'
-  				},
-  				'100%': {
-  					opacity: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'fade-in': 'fade-in 0.5s ease-in forwards',
-  			'fade-out': 'fade-out 0.5s linear forwards'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+        "fade-out": {
+          "0%": {
+            opacity: "1",
+          },
+          "100%": {
+            opacity: "0",
+          },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.5s ease-in forwards",
+        "fade-out": "fade-out 0.5s linear forwards",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
   plugins: [require("tailwindcss-animate")],
-}
-
+};
