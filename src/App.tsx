125ece566b43f4f67d0a67ff74b32b8ee4c6545d
Brian Smiley <briantsmiley42@gmail.com>
2025-01-10 00:10:57 -0500

    fixes app height

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "./components/theme-provider";
 export default function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden px-2">
+      <div className="flex min-h-[100dvh] w-screen flex-col items-center justify-center overflow-hidden px-2">
         <SetGame />
       </div>
     </ThemeProvider>
