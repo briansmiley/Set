44f6785bfdea298e8a81170ea0102667ed1bc7a2
Brian Smiley <briantsmiley42@gmail.com>
2025-01-10 12:40:56 -0500

    changes default theme to light because it looks better

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "./components/theme-provider";
 
 export default function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <div className="flex min-h-[100dvh] w-screen flex-col items-center justify-center overflow-hidden px-2">
         <SetGame />
       </div>
